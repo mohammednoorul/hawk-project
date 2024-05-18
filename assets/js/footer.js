fbff30e5fb936346c038adfc6d58eb4f99ea5bcd
mohammednoorul <138713767+mohammednoorul@users.noreply.github.com>
2024-05-18 07:15:55 +0530

    Create footer.js

---
+++ assets/js/footer.js
@@ -0,0 +1,93 @@
+//start js
+
+let appFooter = `
+
+<!-- Start Footer -->
+<footer class="bg-dark" id="tempaltemo_footer" style="text-align: center;">
+   <div class="container">
+       <div class="row">
+           <div class="col-md-4 pt-5">
+               <h2 class="h2 text-light border-bottom pb-3 border-light logo">Hawk</h2>
+               <ul class="list-unstyled text-light footer-link-list footer-align">
+                   <li class="anchor-hover" style="text-align: left;">
+                       <i class="fas fa-map-marker-alt fa-fw"></i>
+                       &nbsp; No: 168/7, Block B,27th KM Stone, Chennai Bangalore Highway Road, Sembarampakkam, Chennai – 600 123
+                   </li>
+                   <li class="anchor-hover">
+                       <i class="fa fa-phone fa-fw"></i>
+                       <a class="text-decoration-none" href="tel:010-020-0340">&nbsp; +91-9944798371</a>
+                   </li>
+                   <li class="anchor-hover">
+                       <i class="fa fa-envelope fa-fw"></i>
+                       <a class="text-decoration-none" href="mailto:info@company.com">&nbsp; info@company.com</a>
+                   </li>
+               </ul>
+           </div>
+
+           <div class="col-md-4 pt-5">
+               <h2 class="h2 text-light border-bottom pb-3 border-light">Services</h2>
+               <ul class="list-unstyled text-light footer-link-list footer-align">
+                   <li><a class="text-decoration-none anchor-hover" href="Sub_Design.html">Design</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="Sub_Presstools.html">Press tools</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="Sub_Jigs&fixtures.html">Jigs &amp; fixtures</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="Sub_Checkinggauges.html">Checking gauges</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="Sub_SPM.html">SPM</a></li>
+               </ul>
+           </div>
+
+           <div class="col-md-4 pt-5">
+               <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+               <ul class="list-unstyled text-light footer-link-list footer-align">
+                   <li><a class="text-decoration-none anchor-hover" href="./index.html">Home</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="./about.html">About Us</a></li>
+                   <li><a class="text-decoration-none anchor-hover" href="./contact.html">Contact</a></li>
+               </ul>
+           </div>
+
+       </div>
+
+
+           <div  class="row text-light mb-4" id="highlight">
+               <div class="col-12 mb-3">
+                   <div class="w-100 my-3 border-top border-light"></div>
+               </div>
+               <div class="col-auto me-auto">
+                   <ul class="list-inline text-left footer-icons">
+                       <li class="list-inline-item border border-light rounded-circle text-center">
+                           <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                       </li>
+                       <li class="list-inline-item border border-light rounded-circle text-center">
+                           <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                       </li>
+                       <li class="list-inline-item border border-light rounded-circle text-center">
+                           <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                       </li>
+                       <li class="list-inline-item border border-light rounded-circle text-center">
+                           <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+       </div>
+
+
+   <div class="w-100 bg-black py-3">
+       <div class="container ">
+           <div class="row pt-2">
+               <div class="col-12">
+                   <div class="text-left text-light" style="text-align: left; font-size: 15px !important;">
+                       Copyright &copy; 2024 HAWK 
+                       | Designed by <a rel="sponsored" href="https://www.linkedin.com/in/arun-karthik-ab5272170/" target="_blank" style="text-decoration: none; font-size: 15px !important;">A</a> & <a rel="sponsored" href="https://www.linkedin.com/in/mohammed-noorullah-baig/" target="_blank" style="text-decoration: none; font-size: 15px !important;">N</a>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</footer>
+<!-- End Footer -->
+
+
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
+
+// end js
